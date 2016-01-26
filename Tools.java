fb73a40a0c5b869018c25995b3ed90f9116a3810
Daniel Koics <koics.dani@gmail.com>
2016-01-26 11:38:41 +0100

    bugfix: modifier 'static' was missing from the methods of class 'Tools'

--- Tools.java
+++ Tools.java
@@ -6,17 +6,17 @@ public class Tools
 {
 	public static int theAnswer = 42;
 	
-	public String toXML(String tagName, String value)
+	public static String toXML(String tagName, String value)
 	{
 		return "";
 	}
 	
-	public HashMap<Person, Integer> countMoviesPerPerson(List<Movie> movies)
+	public static HashMap<Person, Integer> countMoviesPerPerson(List<Movie> movies)
 	{
 		return new HashMap<Person, Integer>();
 	}
 	
-	public String[] getMovieTitles(List<Movie> movies)
+	public static String[] getMovieTitles(List<Movie> movies)
 	{
 		return new String[0];
 	}
